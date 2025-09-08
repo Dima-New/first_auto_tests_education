919cfe61bac01ae8582b54edb93e45349a5c213a
Dima-New <dmkotliarov@gmail.com>
2025-09-08 17:52:53 +0300

    auto-test switch to alert

---
+++ lesson_2_3_4.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+link = "https://suninjuly.github.io/alert_accept.html"
+browser = webdriver.Chrome()
+
+try:
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+    browser.switch_to.alert.accept()
+    x = browser.find_element(By.CSS_SELECTOR, "#input_value").text
+    y = calc(x)
+    browser.find_element(By.TAG_NAME, "input").send_keys(y)
+    browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+
+finally:
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
