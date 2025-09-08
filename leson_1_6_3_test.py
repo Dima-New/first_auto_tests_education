e62941a047d0f9fa577773a8833373381c2ea3f6
Dima-New <dmkotliarov@gmail.com>
2025-09-08 17:34:57 +0300

    Auto test input

---
+++ leson_1_6_3_test.py
@@ -0,0 +1,30 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+
+def test_selenium():
+    link = "http://suninjuly.github.io/simple_form_find_task.html"
+    global browser
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+
+try:
+    test_selenium()
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    input1.send_keys("Dmytro")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Test")
+    input3 = browser.find_element(By.CLASS_NAME, "city")
+    input3.send_keys("New York")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("USA")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+finally:
+    # закрываем браузер после всех манипуляций
+    time.sleep(5)
+    browser.quit()
