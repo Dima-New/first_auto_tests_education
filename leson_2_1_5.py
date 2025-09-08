7bc1705374ed72fc84cd48c0b4f948c99321df5a
Dima-New <dmkotliarov@gmail.com>
2025-09-08 17:47:34 +0300

    auto-test .text

---
+++ leson_2_1_5.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+link = "https://suninjuly.github.io/math.html"
+browser = webdriver.Chrome()
+
+try:
+    browser.get(link)
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+    input = browser.find_element(By.TAG_NAME, "input")
+    input.send_keys(y)
+    browser.find_element(By.CSS_SELECTOR, "#robotCheckbox").click()
+    browser.find_element(By.CSS_SELECTOR, "#robotsRule").click()
+    browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+
+
+finally:
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
