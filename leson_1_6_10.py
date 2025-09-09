5940dc97ea13ec0e44ee240efd5ab762300dac69
Dima-New <dmkotliarov@gmail.com>
2025-09-09 16:52:24 +0300

    assert text

--- leson_1_6_10.py
+++ leson_1_6_10.py
@@ -29,7 +29,7 @@ try:
     welcome_text = welcome_text_elt.text
 
     # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
+    assert "Congratulations! You have successfully registered!" == welcome_text, "New customer is not registered or recheck successful message"
 
 finally:
     time.sleep(10)
